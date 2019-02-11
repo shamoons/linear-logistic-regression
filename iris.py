a99f8d026f9b735e99518e150445c98402b052b3
Shamoon Siddiqui <shamoon@develop.io>
2019-02-10 19:11:44 -0500

    Rotated matrix

--- iris.py
+++ iris.py
@@ -1,45 +1,34 @@
 import numpy as np
 
-import pandas
-
 def sigmoid(z):
-  return 1/(1+ np.exp(-z))
+  return 1/( 1+ np.exp(-z))
 
-def h(x, w):
+def h(w, x):
   return sigmoid(np.dot(w.T, x))
 
-def cost(X, W, Y):
-  return np.sum(Y * np.log(h(X, W)) + (1 - Y) * np.log(1 - h(X, W))) / (Y.size)
+def gradient(a, y, x):
+  dZ = a - y
+  print(x.shape)
+  print(dZ.shape)
 
-alpha = 0.1
+  return np.dot(x, dZ.T) / y.size
+
 iris_data = np.loadtxt('Iris-binary.csv', delimiter=',')
-inputX = iris_data[:,1:5]
-outputY = iris_data[:,5]
+alpha = 0.1
+Y = iris_data[:,5]
+X = np.ones((5, Y.size))
+X[1,:] = iris_data[:,1]
+X[2,:] = iris_data[:,2]
+X[3,:] = iris_data[:,3]
+X[4,:] = iris_data[:,4]
 
-normalizedX = np.ones((outputY.size, 5))
-normalizedX[:,1] = inputX[:,0]
-normalizedX[:,2] = inputX[:,1]
-normalizedX[:,3] = inputX[:,2]
-normalizedX[:,4] = inputX[:,3]
+W = np.ones((5,1))
 
-weights = np.ones((5,))
-errorDiff = 100
+for i in range(1000):
+  print('Iteration', i)
+  a = h(W, X)
+  dW = gradient(a, Y, X)
+  print(dW)
+  W = W - dW * alpha
 
-for iter in range(1000):
-  currentCost = cost(normalizedX, weights, outputY)
-  if iter % 50 == 0:
-    print(iter, 'iteration', weights)
-    print('Cost', currentCost)
-    print('Error', errorDiff)
-  
-  for i in range(outputY.size):
-    errorDiff = h(normalizedX[i], weights) - outputY[i]
-    weights[0] = weights[0] - (1 / outputY.size) * alpha * (errorDiff) * normalizedX[i][0]
-    weights[1] = weights[1] - (1 / outputY.size) * alpha * (errorDiff) * normalizedX[i][1]
-    weights[2] = weights[2] - (1 / outputY.size) * alpha * (errorDiff) * normalizedX[i][2]
-    weights[3] = weights[3] - (1 / outputY.size) * alpha * (errorDiff) * normalizedX[i][3]
-    weights[4] = weights[4] - (1 / outputY.size) * alpha * (errorDiff) * normalizedX[i][4]
-
-predictions = h(normalizedX, weights)
-
-print(predictions)
+print(W)
